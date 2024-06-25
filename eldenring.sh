1a6090550feef32100c71947c7fadafb6cefc119
TJ-alchemist <tejasvijay09@gmail.com>
2024-06-25 19:48:38 +0530

    modify eldenring file

--- eldenring.sh
+++ eldenring.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
+echo "Welcome. Choose your avatar
+1. Samurai
+2. Prisoner
+3. Prophet
+"
+
+read avatar
+
+case $avatar in
+1)
+	type="Samurai"
+	hp=50
+	attack=5
+	;;
+2)
+	type="Prisoner"
+	hp=40
+	attack=4
+	;;
+3)
+	type="Prophet"
+	hp=60
+	attack=1
+	;;
+esac
+
+echo "You chose to be a $type. Your HP is $hp. Your attack is $attack"
+
+echo ""
+
 echo "The first beast approaches. Pick a number between 0-1."
 read vanquished
 
 beast=$(($RANDOM % 2))
 
-if [[ $vanquished == $beast ]]; then
+if [[ $vanquished == $beast && 50 > 25 ]]; then
 	echo "You vanquished the beast!!!"
 else
 	echo "You died!"
@@ -22,6 +52,8 @@ margot=$(($RANDOM % 10))
 
 if [[ $margot == $vanquished || $vanquished == "cheat" ]]; then
 	echo "Margot has been vanquished"
+elif [[ $USER == "tejas" ]]; then
+	echo "Tejas always wins!!"
 else
 	echo "Margot killed you"
 	exit 1
