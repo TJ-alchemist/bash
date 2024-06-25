b6afeb1b7156cca67f625362124e652bac193f87
TJ-alchemist <tejasvijay09@gmail.com>
2024-06-25 19:28:10 +0530

    modify eldenring file

--- eldenring.sh
+++ eldenring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "The first beast approaches. Pick a number between 0 and 1. (0/1)"
+echo "The first beast approaches. Pick a number between 0-1."
 read vanquished
 
 beast=$(($RANDOM % 2))
@@ -9,4 +9,20 @@ if [[ $vanquished == $beast ]]; then
 	echo "You vanquished the beast!!!"
 else
 	echo "You died!"
+	exit 1
+fi
+
+sleep 2
+
+echo ""
+echo "The second beast margot approaches. Pick a number between 0-9."
+read vanquished
+
+margot=$(($RANDOM % 10))
+
+if [[ $margot == $vanquished || $vanquished == "cheat" ]]; then
+	echo "Margot has been vanquished"
+else
+	echo "Margot killed you"
+	exit 1
 fi
